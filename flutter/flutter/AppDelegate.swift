84853adb556e8d59b94ed511ef7c892bd0d702cb
Dean Liu <deanliu@gmail.com>
2015-02-04 11:50:16 -0800

    Reorganized files and directories.  Cleaned up project settings to reflect the new directory structure.

---
+++ flutter/flutter/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  flutter
+//
+//  Created by Dean Liu on 2/2/15.
+//  Copyright (c) 2015 dragonfly. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+        let lsof_path = NSBundle.mainBundle().pathForResource("lsof", ofType: "sh")
+        println(lsof_path)
+        let sc = SystemCall(cmd: lsof_path!, args: []);
+        let result = sc.run()
+        let lines = result.componentsSeparatedByString("\n");
+        
+        for line in lines{
+            let lsof = Lsof(raw_line: line, delimiter: "~")
+//            println ("\(lsof.name) \(lsof.command)")
+        }
+        
+        let geomanager = GeoIpManager()
+        geomanager.test()
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+}
+
