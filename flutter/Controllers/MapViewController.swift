3a2abc69cff18c706e6dad6e26d8a78c4cc3ba71
Dean Liu <deanliu@gmail.com>
2015-02-28 14:34:39 +0800

    Zoom out to fit pins on selected app.

--- flutter/Controllers/MapViewController.swift
+++ flutter/Controllers/MapViewController.swift
@@ -28,6 +28,9 @@ class MapViewController: NSViewController, MKMapViewDelegate, ConnectionCallback
         } else {
             self.addAnnotations(self.savedLsofLocations)
         }
+        
+        
+        self.zoomToFitMapAnnotations()
     }
     
     func zoomIn(location: Location) {
@@ -37,6 +40,44 @@ class MapViewController: NSViewController, MKMapViewDelegate, ConnectionCallback
         
         self.mapView.setRegion(region, animated: false)
         self.mapView.regionThatFits(region)
+    }
+    
+    func zoomToFitMapAnnotations() {
+        dispatch_async(dispatch_get_main_queue(), {
+            if self.mapView.annotations.count <= 0 {
+                return
+            }
+            
+            var topLeftCoord = CLLocationCoordinate2D(latitude: -90, longitude: 180)
+            var bottomRightCoord = CLLocationCoordinate2D(latitude: 90, longitude: -180)
+            
+            for annotation in self.mapView.annotations {
+                topLeftCoord.longitude = fmin(topLeftCoord.longitude, annotation.coordinate.longitude);
+                topLeftCoord.latitude = fmax(topLeftCoord.latitude, annotation.coordinate.latitude);
+                
+                bottomRightCoord.longitude = fmax(bottomRightCoord.longitude, annotation.coordinate.longitude);
+                bottomRightCoord.latitude = fmin(bottomRightCoord.latitude, annotation.coordinate.latitude);
+            }
+            
+            var coord = CLLocationCoordinate2D(
+                latitude: topLeftCoord.latitude - (topLeftCoord.latitude - bottomRightCoord.latitude) * 0.5,
+                longitude: topLeftCoord.longitude + (bottomRightCoord.longitude - topLeftCoord.longitude) * 0.5
+            )
+
+            var span = MKCoordinateSpan(
+                latitudeDelta: fabs(topLeftCoord.latitude - bottomRightCoord.latitude) * 1.2,
+                longitudeDelta: fabs(bottomRightCoord.longitude - topLeftCoord.longitude) * 1.2
+            )
+            // If only one point, then we add some padding, otherwise it zooms at max level
+            if (span.latitudeDelta <= 0 && span.longitudeDelta <= 0 ) {
+                span.latitudeDelta = 0.25
+                span.longitudeDelta = 0.25
+            }
+            var region = MKCoordinateRegion(center: coord, span: span)
+            
+            region = self.mapView.regionThatFits(region)
+            self.mapView.setRegion(region, animated: false)
+        })
     }
     
     // ConnectionOperations delegate callback method
@@ -100,6 +141,7 @@ class MapViewController: NSViewController, MKMapViewDelegate, ConnectionCallback
     // Move queue operation somewhere else?
     func queueOperation() {
         let co = ConnectionOperation()
+        co.threadPriority = 0.01
         co.queuePriority = NSOperationQueuePriority.VeryLow
         co.qualityOfService = NSQualityOfService.Background
         co.delegates.append(self)
